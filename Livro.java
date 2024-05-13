c1c00dacf875fda6c32b6c78745b0305b1668af4
꧁𓊈𒆜🅺🅰🆁🅴🅽𒆜𓊉꧂ <karenoliveira.lc@gmail.com>
2024-05-13 14:01:57 -0300

    Livro

---
+++ Livro.java
@@ -0,0 +1,27 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    
+    // Construtor
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    
+    public void imprimir() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+    }
+}
---
+++ TesteLivro.java
@@ -0,0 +1,10 @@
+public class TesteLivro {
+    public static void main(String[] args) {
+        Livro livro = new Livro("Drácula", "Bram Stoker\n");
+        
+        System.out.println("\nInformações do Livro:");
+        livro.imprimir();
+        
+    }
+}
+
